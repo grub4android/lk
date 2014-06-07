2bc94f122224e9bf5ee725d7a3746881c5027dce
M1cha <sigmaepsilon92@gmail.com>
2014-06-07 22:46:14 +0200

    [lib][ext4] add mmc blockdev driver

--- lib/ext4/rules.mk
+++ lib/ext4/rules.mk
@@ -15,14 +15,16 @@ EXT4_SRCS = \
 	ext4_ialloc.c \
 	ext4_balloc.c \
 	ext4_blockdev.c \
-	ext4_inode.c
+	ext4_inode.c \
+	blockdev/ext4_mmcdev.c
 
 
 EXT4_OBJS = $(EXT4_SRCS:%.c=%.o)
 
 INCLUDES += \
 	-I. \
-	-I$(EXT4_LOCAL_PATH)
+	-I$(EXT4_LOCAL_PATH) \
+	-I$(EXT4_LOCAL_PATH)/blockdev
 
 OBJS += $(addprefix $(EXT4_LOCAL_PATH)/, $(EXT4_OBJS))
 
