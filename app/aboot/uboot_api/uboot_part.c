63f37fe5a557b5ed7dcb2795facb24d518d3e49c
M1cha <sigmaepsilon92@gmail.com>
2014-08-18 11:38:49 +0200

    uboot_api: part: add virtual mmc device which represents the tar archive where we store GRUB

--- app/aboot/uboot_api/uboot_part.c
+++ app/aboot/uboot_api/uboot_part.c
@@ -11,6 +11,7 @@
 #include <malloc.h>
 #include <mmc.h>
 #include <partition_parser.h>
+#include <lib/tar.h>
 
 #include "uboot_part.h"
 
@@ -21,28 +22,46 @@ static int initialized = 0;
 static block_dev_desc_t mmcdev = {
 	.type = DEV_TYPE_HARDDISK,
 
-	.lba = 128,
+	.lba = 0,
 	.blksz = BLOCK_SIZE,
-	
+
+	.block_read = block_read,
+	.block_write = block_write,
+};
+
+static block_dev_desc_t tardev = {
+	.type = DEV_TYPE_HARDDISK,
+
+	.lba = 0,
+	.blksz = BLOCK_SIZE,
+
 	.block_read = block_read,
 	.block_write = block_write,
 };
 
 static unsigned long block_read(int dev, lbaint_t start, lbaint_t blkcnt, void *buffer) {
+	// NAND
 	if(dev==0) {
 		return mmc_read(start*BLOCK_SIZE, buffer, blkcnt*BLOCK_SIZE);
+	}
+	// BOOT-TAR
+	else if(dev==1) {
+		return grub_tar_read(grub_tar_get_tio(), start, blkcnt, buffer);
 	}
 
 	return 0;
 }
 
 static unsigned long block_write(int dev, lbaint_t start, lbaint_t blkcnt, const void *buffer) {
-
 	return 0;
 }
 
 static int initialize(void) {
+	struct tar_io *tio = grub_tar_get_tio();
+
 	mmcdev.lba = (mmc_get_device_capacity()) / BLOCK_SIZE;
+	tardev.lba = tio->lba;
+
 	return 0;
 }
 
@@ -50,8 +69,11 @@ block_dev_desc_t *get_dev(const char *ifname, int dev) {
 	if(!initialized && initialize())
 		return NULL;
 
-	if(strcmp(ifname, "mmc")==0 && dev==0) {
-		return &mmcdev;
+	if(strcmp(ifname, "mmc")==0) {
+		// NAND
+		if(dev==0) return &mmcdev;
+		// BOOT-TAR
+		else if(dev==1) return &tardev;
 	}
 
 	return NULL;
