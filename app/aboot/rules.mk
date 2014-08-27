7d4f89d28b8093cc56d23af9db10544a3b3518dc
M1cha <sigmaepsilon92@gmail.com>
2014-08-27 11:25:55 +0200

    [app][aboot] define BOOT_2NDSTAGE if 2ndstage boot is enabled
    Conflicts:
    	app/aboot/rules.mk

--- app/aboot/rules.mk
+++ app/aboot/rules.mk
@@ -2,6 +2,10 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
 
+ifeq ($(ENABLE_2NDSTAGE_BOOT),1)
+GLOBAL_DEFINES += BOOT_2NDSTAGE=1
+endif
+
 MODULE_SRCS += \
 	$(LOCAL_DIR)/aboot.c \
 	$(LOCAL_DIR)/recovery.c
