928d910aeca91e0f9bf55967fffe9b827174d566
M1cha <sigmaepsilon92@gmail.com>
2014-08-27 11:25:55 +0200

    app: aboot: define BOOT_2NDSTAGE if 2ndstage boot is enabled

--- app/aboot/rules.mk
+++ app/aboot/rules.mk
@@ -12,6 +12,10 @@ ifneq ($(GRUB_BOOT_PARTITION),)
 CFLAGS += -DGRUB_BOOT_PARTITION=\"$(GRUB_BOOT_PARTITION)\"
 endif
 
+ifeq ($(ENABLE_2NDSTAGE_BOOT),1)
+DEFINES += BOOT_2NDSTAGE=1
+endif
+
 OBJS += \
 	$(LOCAL_DIR)/aboot.o \
 	$(LOCAL_DIR)/fastboot.o \
