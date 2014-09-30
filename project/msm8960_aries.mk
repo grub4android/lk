b5eeca6b80f5c785506345e69da81cf9180bbcea
M1cha <sigmaepsilon92@gmail.com>
2014-09-30 12:55:35 +0200

    project: msm8960_aries: include common msm8960 config to reduce redundancy

--- project/msm8960_aries.mk
+++ project/msm8960_aries.mk
@@ -1,27 +1,6 @@
 # top level project rules for the msm8960_virtio project
 #
 LOCAL_DIR := $(GET_LOCAL_DIR)
-
+include $(LOCAL_DIR)/msm8960.mk
 TARGET := msm8960_aries
-
-MODULES += app/aboot
-
-DEBUG := 1
-EMMC_BOOT := 1
-
-#DEFINES += WITH_DEBUG_DCC=1
-DEFINES += WITH_DEBUG_UART=1
-DEFINES += WITH_DEBUG_LOG_BUF=1
-#DEFINES += WITH_DEBUG_FBCON=1
-DEFINES += WITH_XIAOMI_DUALBOOT=1
 SPLASH_PARTITION_NAME:=\"logo\"
-
-#Disable thumb mode
-#TODO: The gold linker has issues generating correct
-#thumb interworking code for LK. Confirm that the issue
-#is with the linker and file a bug rep
-ENABLE_THUMB := false
-
-ifeq ($(EMMC_BOOT),1)
-DEFINES += _EMMC_BOOT=1
-endif
