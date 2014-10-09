0937aa4cff2d5aa024747f19039a2826931ec577
M1cha <sigmaepsilon92@gmail.com>
2014-10-09 13:54:12 +0200

    [project][msm8960] add some defines from CAF

--- project/msm8960.mk
+++ project/msm8960.mk
@@ -7,8 +7,20 @@ TARGET := msm8960
 MODULES += \
 	app/tests \
 	app/stringtests \
-	app/shell
+	app/shell \
+	app/fastboot
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+DEBUG := 0
+else
+DEBUG := 1
+endif
 
 #GLOBAL_DEFINES += WITH_DEBUG_DCC=1
 GLOBAL_DEFINES += WITH_DEBUG_UART=1
 #GLOBAL_DEFINES += WITH_DEBUG_FBCON=1
+
+EMMC_BOOT := 1
+ifeq ($(EMMC_BOOT),1)
+GLOBAL_DEFINES += _EMMC_BOOT=1
+endif
