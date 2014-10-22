b915786e7b42f86270c95b8ad5685070a8156803
M1cha <sigmaepsilon92@gmail.com>
2014-10-22 23:44:52 +0200

    [app][menu][menu_settings] make local functions static and use naming scheme from main menu

--- app/menu/menu_settings.c
+++ app/menu/menu_settings.c
@@ -11,54 +11,54 @@
 
 #include "menu_private.h"
 
-void menu_back(void) {
+static void menu_exec_back(void) {
 	menu_leave();
 }
 
 #if WITH_XIAOMI_DUALBOOT
-void menu_exec_dualbootmode(void) {
+static void menu_exec_dualbootmode(void) {
 	set_dualboot_mode(dual_boot_sign==DUALBOOT_BOOT_SECOND?DUALBOOT_BOOT_FIRST:DUALBOOT_BOOT_SECOND);
 }
-void menu_format_dualbootmode(char** buf) {
+static void menu_format_dualbootmode(char** buf) {
 	*buf = calloc(100, 1);
 	snprintf(*buf, 100, "    Dualboot mode [%s]", dual_boot_sign==DUALBOOT_BOOT_SECOND?"System2":"System1");
 }
 #endif
 
-void menu_chargerscreen(void) {
+static void menu_exec_chargerscreen(void) {
 	device.charger_screen_enabled = !device.charger_screen_enabled;
 	write_device_info(&device);
 }
-void menu_chargerscreen_format(char** buf) {
+static void menu_format_chargerscreen(char** buf) {
 	*buf = calloc(100, 1);
 	snprintf(*buf, 100, "    Charger Screen [%s]", device.charger_screen_enabled?"enabled":"disabled");
 }
 
-void menu_splash(void) {
+static void menu_exec_splash(void) {
 	device.use_splash_partition = !device.use_splash_partition;
 	write_device_info(&device);
 }
-void menu_splash_format(char** buf) {
+static void menu_format_splash(char** buf) {
 	*buf = calloc(100, 1);
 	snprintf(*buf, 100, "    Splash Logo [%s]", device.use_splash_partition?"enabled":"disabled");
 }
 
-void menu_forcefastboot(void) {
+static void menu_exec_forcefastboot(void) {
 	device.force_fastboot = !device.force_fastboot;
 	write_device_info(&device);
 }
-void menu_forcefastboot_format(char** buf) {
+static void menu_format_forcefastboot(char** buf) {
 	*buf = calloc(100, 1);
 	snprintf(*buf, 100, "    Force Fastboot Mode [%s]", device.force_fastboot?"enabled":"disabled");
 }
 
 struct menu_entry entries_settings[] = {
-	{"    <-- Back", &menu_back, NULL},
+	{"    <-- Back", &menu_exec_back, NULL},
 #if WITH_XIAOMI_DUALBOOT
 	{"", &menu_exec_dualbootmode, &menu_format_dualbootmode},
 #endif
-	{"", &menu_chargerscreen, &menu_chargerscreen_format},
-	{"", &menu_splash, &menu_splash_format},
-	{"", &menu_forcefastboot, &menu_forcefastboot_format},
+	{"", &menu_exec_chargerscreen, &menu_format_chargerscreen},
+	{"", &menu_exec_splash, &menu_format_splash},
+	{"", &menu_exec_forcefastboot, &menu_format_forcefastboot},
 	{NULL,NULL,NULL},
 };
