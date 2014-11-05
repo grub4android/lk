a8f6eab09de332a9db8eb168937aa3eab67fefa1
M1cha <sigmaepsilon92@gmail.com>
2014-11-05 16:37:49 +0100

    [target][msm8960_aries][init] fix battery charge detection

--- target/msm8960_aries/init.c
+++ target/msm8960_aries/init.c
@@ -244,7 +244,8 @@ unsigned check_reboot_mode(void)
 
 unsigned target_pause_for_battery_charge(void)
 {
-	if (target_check_power_on_reason() == PWR_ON_EVENT_WALL_CHG)
+	unsigned po_reason = target_check_power_on_reason();
+	if (po_reason == PWR_ON_EVENT_WALL_CHG || po_reason == PWR_ON_EVENT_USB_CHG)
 		return 1;
 
 	return 0;
