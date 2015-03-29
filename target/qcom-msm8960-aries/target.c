f8741210415e3703ffa191d752bb5e2d8b5cf855
M1cha <sigmaepsilon92@gmail.com>
2015-03-29 22:15:50 +0200

    [target][qcom-msm8960-aries] add support for UART switch

--- target/qcom-msm8960-aries/target.c
+++ target/qcom-msm8960-aries/target.c
@@ -22,8 +22,58 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <debug.h>
 #include <platform/qcom.h>
+#include <platform/gpio.h>
+#include <platform/gsbi.h>
+#include <platform/board.h>
 #include <platform/msm_panel.h>
+
+#ifdef QCOM_ENABLE_UART
+#define MSM_BOOT_UART_SWITCH_GPIO_MITWOA	33
+#define MSM_BOOT_UART_SWITCH_GPIO_MITWO		62
+
+int target_uart_gpio_config(uint8_t id) {
+	// configure UART switch
+	if(board_target_id() == LINUX_MACHTYPE_8960_CDP)
+	{
+		gpio_tlmm_config(MSM_BOOT_UART_SWITCH_GPIO_MITWOA, 0, GPIO_OUTPUT,
+				GPIO_NO_PULL, GPIO_16MA, GPIO_ENABLE);
+		gpio_direction(MSM_BOOT_UART_SWITCH_GPIO_MITWOA, GPIO_OUTPUT);
+		gpio_set(MSM_BOOT_UART_SWITCH_GPIO_MITWOA, GPIO_IN_OUT_HIGH);
+
+	} else if (board_target_id() == LINUX_MACHTYPE_8064_MTP
+			|| board_target_id() == LINUX_MACHTYPE_8064_MITWO) {
+		gpio_tlmm_config(MSM_BOOT_UART_SWITCH_GPIO_MITWO, 0, GPIO_OUTPUT,
+				GPIO_NO_PULL, GPIO_16MA, GPIO_ENABLE);
+		gpio_direction(MSM_BOOT_UART_SWITCH_GPIO_MITWO, GPIO_OUTPUT);
+		gpio_set(MSM_BOOT_UART_SWITCH_GPIO_MITWO, GPIO_IN_OUT_HIGH);
+	}
+
+	if(board_platform_id() == MPQ8064 || board_platform_id() == MSM8260AB)
+	{
+		switch (id) {
+
+		case GSBI_ID_5:
+			/* configure rx gpio */
+			gpio_tlmm_config(23, 1, GPIO_INPUT, GPIO_NO_PULL,
+							 GPIO_8MA, GPIO_DISABLE);
+			/* configure tx gpio */
+			gpio_tlmm_config(22, 1, GPIO_OUTPUT, GPIO_NO_PULL,
+							 GPIO_8MA, GPIO_DISABLE);
+			break;
+
+		default:
+			ASSERT(0);
+		}
+
+		return 1;
+	}
+
+	// fall back to platform code
+	return 0;
+}
+#endif
 
 void target_early_init(void)
 {
